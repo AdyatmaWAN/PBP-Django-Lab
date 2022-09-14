da4bbd34231a7550ec0687549887230cfc840722
AdyatmaNugrahaYudha <awan2403@gmail.com>
2022-09-14 22:11:59 +0700

    Membuat page untuk menampilkan data bentuk xml dan json.

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from wishlist.views import show_wishlist
+from wishlist.views import show_data_json
+from wishlist.views import show_data_xml
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('json/', show_data_json, name='show_data_json'),
+    path('xml/', show_data_xml, name='show_data_xml'),
 ]
